36e0d700e35db5134a780ec3df4bc4e92297b7d6
Ankaks <ankit.dubeyit02@gmail.com>
2017-02-15 11:24:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# LearnSH
-
-
+# ShellScripting
---
+++ index.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Choose Options to see Shell example "
+echo "1. Loops"
+echo "2. Functions"
+echo "3. Exit Status"
+echo "4. Variables"
+echo "5. Arrays"
+echo "6. awk"
+echo "7. sed"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
